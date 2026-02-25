3a254afac2375128c7a109c2085782856ec0e897
santusht06 <115890693+santusht06@users.noreply.github.com>
2026-02-25 19:25:05 +0530

    Section divided

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import Navbar from "../components/Navbar";
 import Hero from "../components/Hero";
 import Hero2 from "../components/Hero2";
+import HeroCard from "../components/HeroCard";
 
 const Home = () => {
   return (
     <>
       <Hero />
       <Hero2 />
+      <HeroCard />
     </>
   );
 };
