d5013a1877b772772c687762040516891974c364
santusht06 <115890693+santusht06@users.noreply.github.com>
2026-02-27 21:07:12 +0530

    minor changes

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -4,6 +4,7 @@ import Hero from "../components/Hero";
 import Hero2 from "../components/Hero2";
 import HeroCard from "../components/HeroCard";
 import Hero3 from "../components/Hero3";
+import Hero4 from "../components/Hero4";
 
 const Home = () => {
   return (
