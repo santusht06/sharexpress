69805c9c4958ee54645db8aa3918ae79f32e880a
santusht06 <115890693+santusht06@users.noreply.github.com>
2026-03-07 20:44:22 +0530

    USER AUTH FLOW V1

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,21 +1,27 @@
 import React from "react";
 import LOGOw from "../../../DOCUMENTS/logo.PNG";
+import { useSelector } from "react-redux";
 
 const Dashboard = () => {
+  const { user } = useSelector((state) => state.auth);
+  console.log(user?.user_name || null);
+
   return (
     <div className="w-screen h-screen bg-black">
       {/* LEFT SIDEBAR */}
-      <div className="fixed left-0 top-0 w-[260px] h-screen px-6 py-4  bg-black">
+      <div className="fixed left-0 top-0 w-[260px] h-screen px-6 py-4 bg-black flex flex-col">
+        {/* Logo */}
         <img
           src={LOGOw}
           alt="sharexpress logo"
           className="h-12 object-contain"
         />
 
+        {/* Menu */}
         <div className="mt-8">
           <h1 className="text-[#b3b3b3] text-xs">Uploads</h1>
 
-          <div className="mt-3 flex flex-col gap-1   ">
+          <div className="mt-3 flex flex-col gap-1">
             <div className="rounded-xl bg-[#202020] text-sm px-4 py-2 text-white">
               Files
             </div>
@@ -26,8 +32,17 @@ const Dashboard = () => {
           </div>
         </div>
 
-        <div className="text-white fixed bottom-2 ">
-          <div></div>
+        {/* PROFILE (Bottom) */}
+        <div className="mt-auto pt-6 border-t border-[#ffffff1a]">
+          <div className="text-white text-sm px-2 py-2 hover:bg-[#202020] rounded-lg cursor-pointer flex items-center gap-3    ">
+            <div>
+              <h1> {user?.user_name || null} </h1>
+              <h1 className="text-xs text-[#b3b3b3] ">
+                {" "}
+                {user?.email || null}{" "}
+              </h1>
+            </div>
+          </div>
         </div>
       </div>
 
