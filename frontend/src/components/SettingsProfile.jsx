5fa945e12a8a88aba62c4381715a4167cb7f3780
santusht06 <115890693+santusht06@users.noreply.github.com>
2026-03-07 21:52:15 +0530

    minor changes

--- frontend/src/components/SettingsProfile.jsx
+++ frontend/src/components/SettingsProfile.jsx
@@ -4,20 +4,20 @@ import { useState, useRef, useEffect } from "react";
 
 const SettingsProfile = () => {
   return (
-    <div className="w-48 bg-[#171717] border border-[#ffffff10] rounded-xl shadow-xl py-2">
-      <button className="w-full flex items-center gap-3 px-4 py-2 text-sm text-white hover:bg-[#262626] transition">
+    <div className="w-48 bg-[#171717] border border-[#ffffff10] rounded-xl shadow-xl py-2 px-1 ">
+      <button className="w-full rounded-xl flex items-center gap-3 px-4 py-2 text-sm text-white hover:bg-[#262626] transition">
         <FiUser size={16} />
         Profile
       </button>
 
-      <button className="w-full flex items-center gap-3 px-4 py-2 text-sm text-white hover:bg-[#262626] transition">
+      <button className="w-full rounded-xl  flex items-center gap-3 px-4 py-2 text-sm text-white hover:bg-[#262626] transition">
         <FiSettings size={16} />
         Settings
       </button>
 
-      <div className="border-t border-[#ffffff10] my-2"></div>
+      <div className="border-t rounded-xl  border-[#ffffff10] my-2"></div>
 
-      <button className="w-full flex items-center gap-3 px-4 py-2 text-sm text-white hover:bg-[#262626] transition">
+      <button className="w-full rounded-xl  flex items-center gap-3 px-4 py-2 text-sm text-white hover:bg-[#262626] transition">
         <FiLogOut size={16} />
         Logout
       </button>
