0d27e07f6661c6a9fbc49da856e804f7856c3972
santusht06 <115890693+santusht06@users.noreply.github.com>
2026-04-26 23:35:36 +0530

    DOCKER 2

--- backend/main.py
+++ backend/main.py
@@ -71,14 +71,14 @@ app.add_middleware(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
+        "http://192.168.29.104:4000",
         "http://localhost:5173",
         "http://127.0.0.1:5173",
         "http://localhost:3000",
     ],
     allow_credentials=True,
-    allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
+    allow_methods=["*"],
     allow_headers=["*"],
-    expose_headers=["*"],
 )
 
 
